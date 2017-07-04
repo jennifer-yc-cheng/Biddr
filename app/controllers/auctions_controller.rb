388b5df874854e7c81a175f76d491c939520c134
Jennifer Cheng <ashowga@gmail.com>
2017-07-04 14:15:42 -0700

    create auctions

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,6 +1,36 @@
 class AuctionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+  before_action :find_acution, only: [:show]
 
   def index
   end
+
+  def new
+    @auction = Auction.new
+  end
+
+  def create
+    # render json: auction_params
+    @auction = Auction.new auction_params
+    @auction.user = current_user
+    if @auction.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def auction_params
+    params.require(:auction).permit(:title, :details, :end_date, :reserve_price)
+  end
+
+  def find_auction
+    @auction = Auction.find params[:id]
+  end
+
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,21 @@
 class Auction < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :details, presence: true, length: {minimum: 10}
+  validates :reserve_price, numericality: { greater_than: 1}
+  validate :end_date_cannot_be_in_the_past
+
+
+  def end_date_cannot_be_in_the_past
+    if end_date.present? && end_date < Date.today
+      errors.add(:end_date, "can't be in the past")
+    end
+  end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.start_price ||=1
+  end
 end
